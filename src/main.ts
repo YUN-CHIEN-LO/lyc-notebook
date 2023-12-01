492b3cdae1e211b0b45e6d6e0621aeda272ffa36
YUN-CHIEN-LO <yunchienlo@gmail.com>
2023-12-01 23:14:18 +0800

    chore: 配置 firebase

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,20 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+import { VueFire, VueFireAuth } from "vuefire";
+
+// the file we created above with `database`, `firestore` and other exports
+import { firebaseApp } from "./plugins/firebase";
+
+const app = createApp(App);
+app.use(VueFire, {
+  // imported above but could also just be created here
+  firebaseApp,
+  modules: [
+    // we will see other modules later on
+    VueFireAuth(),
+  ],
+});
+
+app.mount("#app");
